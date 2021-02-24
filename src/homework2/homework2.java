61de7d3354ab4bdeac68e106509c60a52fb10af2
arslanch22 <achaudhry415@gmail.com>
2021-02-23 20:13:33 -0500

    Initial commit

---
+++ src/homework2/homework2.java
@@ -0,0 +1,23 @@
+package homework2;
+
+
+public class homework2 {
+    public static void main(String[] args) {
+
+        byte dailyHours = 24;
+        short daysInYear = 365;
+        int amzonEmployees = 1000000;
+        double intrstRate = 1.5;
+        double accBalance = 105.50;
+        boolean sunRiseFrmWest = false;
+        char firstNmeIn = 'A';
+        char lastNmeIn = 'C';
+
+        System.out.println("In ond day there are " + daysInYear + " days in a year \n and " + dailyHours + " hours in a day");
+        System.out.println("The company amazon has " + amzonEmployees + " employees.");
+        System.out.println("The interest rate for my car is " + intrstRate +  " percent apr \n and my acc balance is " + accBalance);
+        System.out.println("Does the sun rise from the west " + sunRiseFrmWest);
+        System.out.println("The initial for my first name is " + firstNmeIn + " and my last name initial is " + lastNmeIn);
+
+    }
+}
